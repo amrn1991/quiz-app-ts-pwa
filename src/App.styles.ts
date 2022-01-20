3687d72c00bd875271a67b0893f24490890e8e63
amrn1991 <amrfst91@gmail.com>
2022-01-20 11:07:11 +0330

    first commit

---
+++ src/App.styles.ts
@@ -0,0 +1,59 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+    *{
+        box-sizing: border-box;
+        font-family: "Catamaran", sans-serif;
+    }
+    html{
+        height: 100%;
+    }
+    body{
+        background: url('/scenery.jpg');
+        background-size: cover;
+        margin: 0;
+        padding: 0 20px;
+        display: flex;
+        justify-content: center;
+    }
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  > p {
+    color: #fff;
+  }
+
+  .score {
+    color: #109648;
+    margin: 0;
+    font-size: 2rem;
+  }
+  h1 {
+    font-family: Fascinate Inline, Haettenschweiler, 'Arial Narrow Bold',
+      sans-serif;
+    background-clip: text;
+    filter: drop-shadow(2px 2px #ec9f05);
+    color: #01295f;
+    font-size: 70px;
+    margin: 20px;
+    text-align: center;
+  }
+  .start,
+  .next {
+    cursor: pointer;
+    background: linear-gradient(180deg, #fff, #ffcc91);
+    border: 2px solid #d38558;
+    box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.25);
+    border-radius: 10px;
+    height: 40px;
+    margin: 20px 0;
+    padding: 0 40px;
+  }
+  .start {
+      max-width: 200px;
+  }
+`;
---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { ButtonWrapper } from '../QuestionCard.styles';
+import { AnswerObject } from '../App';
+
+type Props = {
+  question: string;
+  answers: string[];
+  callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  userAnswer: AnswerObject | undefined;
+  questionNum: number;
+  totalQuestions: number;
+};
+
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNum,
+  totalQuestions,
+}) => {
+  return (
+    <div>
+      <p className="number">
+        Question: {questionNum} / {totalQuestions}
+      </p>
+      <p dangerouslySetInnerHTML={{ __html: question }} />
+      {answers.map((answer, idx) => (
+        <ButtonWrapper
+          key={idx}
+          isCorrect={userAnswer?.correctAnswer === answer}
+          userClicked={userAnswer?.answer === answer}
+        >
+          <button disabled={!!userAnswer} value={answer} onClick={callback}>
+            <span dangerouslySetInnerHTML={{ __html: answer }} />
+          </button>
+        </ButtonWrapper>
+      ))}
+    </div>
+  );
+};
+
+export default QuestionCard;
